d03ed88e9b0254f9feadb9948c645838e1e79d03
Olaleye Alex-Oni <olaleyealex-oni@olaleyes-mbp.home>
2024-03-04 16:26:33 +0000

    simulates selecting instance type via provided list

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "phunkyec2" {
   ami = "ami-0171207a7acd2a570" # hardcoded stuff
-  instance_type = var.types["eu-west-3"] # using the key value in map to reference instance type
+  instance_type = var.list[1] # using the key value in map to reference instance type
 }
 
 
