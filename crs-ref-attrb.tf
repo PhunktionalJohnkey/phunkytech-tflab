7f4fd511cf8005655c7642a309dc99b53a013cb4
Olaleye Alex-Oni <olaleyealex-oni@olaleyes-mbp.home>
2024-03-04 13:28:00 +0000

    replaces hardcoded values with variables

--- crs-ref-attrb.tf
+++ crs-ref-attrb.tf
@@ -9,24 +9,24 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "app_port" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "172.31.16.4/32"
-  from_port         = 8080
+  cidr_ipv4         = var.vpn_ip
+  from_port         = var.app_port
   ip_protocol       = "tcp"
-  to_port           = 8080
+  to_port           = var.app_port
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ssh_port" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "172.31.16.4/32"
-  from_port         = 22
+  cidr_ipv4         = var.vpn_ip
+  from_port         = var.ssh_port
   ip_protocol       = "tcp"
-  to_port           = 22
+  to_port           = var.ssh_port
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ftp_port" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "172.31.16.4/32"
-  from_port         = 21
+  cidr_ipv4         = var.vpn_ip
+  from_port         = var.ftp_port
   ip_protocol       = "tcp"
-  to_port           = 21
+  to_port           = var.ftp_port
 }
